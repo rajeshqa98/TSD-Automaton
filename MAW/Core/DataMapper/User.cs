cb24f56fab12deefb9fa2f316c636a1f8381e3f1
Sukhjinder <mrsukhjindersukh@gmail.com>
2021-08-04 09:28:15 +0530

    Excel Test data reader implemented

---
+++ MAW/Core/DataMapper/User.cs
@@ -0,0 +1,47 @@
+﻿using MAW.Core.Utils;
+using Npoi.Mapper;
+using Npoi.Mapper.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TSDHybridFramework.Core.DataMapper
+{
+    class User
+    {
+        [Column("UserName")]
+        public string Name { get; set; }
+        public string Pass   { get; set; }
+        public string Url { get; set; }
+
+
+
+        public static List<User> GetFromExcel() {
+            Excel excel = new Excel("Data");
+            List<RowInfo<User>> userRows = excel.Reader().Take<User>().ToList();
+            List<User> users = new List<User>();
+
+            foreach (var user in userRows) {
+                users.Add(user.Value);
+            }
+
+            return users;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+    }
+}
--- MAW/Core/Utils/Excel.cs
+++ MAW/Core/Utils/Excel.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿
+using Npoi.Mapper;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using TSDHybridFramework.Core.DataMapper;
+
 
 namespace MAW.Core.Utils
 {
     class Excel
     {
+        private string excelFilePath { get; set; }
+        private Mapper excel;
+
+
+        public Excel(String fileName) {
+            var path = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            var actualPath = path.Substring(0, path.LastIndexOf("bin"));
+            var projectPath = new Uri(actualPath).LocalPath;
+            var excelFilePath = projectPath + String.Format("TestData\\{0}.xlsx", fileName); ;
+            excel = new Mapper(excelFilePath);
+        }
+
+        public Mapper Reader() {
+            return excel;
+        }
+
+        public void ReadSheet(String sheetName) {
+            //Solution for System.NotSupportedException: No data is available for encoding 1252
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            System.Collections.Generic.List<RowInfo<User>> users = this.Reader().Take<User>().ToList();
+            
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            List<User> users =User.GetFromExcel();
+
+            foreach (User user in users) {
+                Console.WriteLine(user.Name);
+                Console.WriteLine(user.ToString());
+
+
+            }
+
+        }
     }
 }
