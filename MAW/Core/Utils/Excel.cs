383eadbe5ed9ccdc127153069af3a6ccd51aeb5e
Sukhjinder <mrsukhjindersukh@gmail.com>
2021-08-05 10:10:04 +0530

    Excel get data as List of Rows implemented

--- MAW/Core/Utils/Excel.cs
+++ MAW/Core/Utils/Excel.cs
@@ -1,7 +1,11 @@
 ﻿
+using AngleSharp.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Npoi.Mapper;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -29,26 +33,122 @@ namespace MAW.Core.Utils
             return excel;
         }
 
-        public void ReadSheet(String sheetName) {
-            //Solution for System.NotSupportedException: No data is available for encoding 1252
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        private static bool IsValidJson(string strInput)
+        {
+            if (string.IsNullOrWhiteSpace(strInput)) { return false; }
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    //Exception in parsing json
+                    Console.WriteLine(jex.Message);
+                    return false;
+                }
+                catch (Exception ex) //some other exception
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-            System.Collections.Generic.List<RowInfo<User>> users = this.Reader().Take<User>().ToList();
+        public List<Dictionary<string, string>> ReadSheet(String sheetName) {
+
+            List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
+
+
+            List<RowInfo<dynamic>> list
+                = this.Reader().Take<dynamic>(sheetName).ToList();
+
+            NPOI.SS.UserModel.ISheet sheet
+                = GetSheet(sheetName);
+
+            List<string> headerValues = new List<string>(); 
+            //Header Row
+            NPOI.SS.UserModel.IRow row
+                = sheet.GetRow(0);
+
+            int totalCells = row.LastCellNum;
+            int lastRow = sheet.LastRowNum;
+
+            for (int i = 0; i < totalCells; i++) {
+                headerValues.Add(row.GetCell(i).StringCellValue);
+            }
+
             
+
+            for (int r = 1; r < lastRow; r++) {
+                Dictionary<string, string> rowKeyValuePairs = new Dictionary<string, string>();
+
+                for (int cell = 0; cell < totalCells; cell++) {
+                    rowKeyValuePairs.Add(headerValues[cell].ToUpper().Trim(),
+                        sheet.GetRow(r).GetCell(cell).StringCellValue
+                        );
+                }
+
+                rows.Add(rowKeyValuePairs);
+            }
+
+            return rows;   
+        }
+
+        public NPOI.SS.UserModel.ISheet GetSheet(string sheetName)
+        {
+
+            if (isExistSheet(sheetName))
+            {
+                return this.Reader().Workbook.GetSheet(sheetName);
+            }
+            else {
+                return null;
+           }
 
 
         }
-
+        private bool isExistSheet(string sheetName)
+        {
+            NPOI.SS.UserModel.ISheet sheet = this.Reader().Workbook.GetSheet(sheetName);
+            if (sheet != null)
+            {
+                sheet.ForceFormulaRecalculation = true;
+                return true;
+            }
+            else
+                return false;
+        }
         static void Main(string[] args)
         {
 
-            List<User> users =User.GetFromExcel();
+            /* List<User> users =User.GetFromExcel();
 
-            foreach (User user in users) {
-                Console.WriteLine(user.Name);
-                Console.WriteLine(user.ToString());
+             foreach (User user in users) {
+                 Console.WriteLine(user.Name);
+                 Console.WriteLine(user.ToString());
 
 
+             }*/
+
+            Excel excel = new Excel("Data");
+            List<Dictionary<string, string>> rows
+                = excel.ReadSheet("User");
+
+            Console.WriteLine(rows.Count);
+             
+
+            foreach (KeyValuePair<string, string> ele2 in rows[1])
+            {
+                Console.WriteLine("{0} : {1}", ele2.Key, ele2.Value);
             }
 
         }
